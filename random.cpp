5711f62a5558943eaa6505d99a80719687a707e3
Keshav Saini <72993616+sainikeshav@users.noreply.github.com>
2021-09-16 18:58:21 +0530

    Delete random.cpp

--- random.cpp
+++
@@ -1,9 +0,0 @@
-/*	Explaining the working of the random function
-		random function:
-							randnum = rand() % max + initial_value
-		In general, range of a randum function is,  initial_value  to  (initial_value + max - 1)
-		In this function value of initial value = 97
-								& max = 26
-						so range will be 97 to (97 + 26 - 1)   i.e.  97 to 122
-		In ASCII table small alphabets are from 97 to 122
-*/
